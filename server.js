607790c5296efa1382a24dafb94f893ae5e0e289
oussama hamdaoui <oussamahamdaoui@live.fr>
2019-05-19 01:38:02 +0200

    allow origin

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-
+io.set('origins', '*:*');
 io.on('connection', (socket) => {
   socket.on('message', (msg) => {
     socket.broadcast.emit('message', msg);
