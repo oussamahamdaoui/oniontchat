ccc1157d057a07e9c0e867d1a589a3bd2b89298a
oussama hamdaoui <oussamahamdaoui@live.fr>
2019-05-19 02:34:24 +0200

    Add cool face API

--- server.js
+++ server.js
@@ -2,22 +2,28 @@ const app = require('express')();
 const http = require('http');
 const socketio = require('socket.io');
 
+try {
 
-app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
-        res.header("Access-Control-Allow-Headers", "Content-Type");
-        res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-        next();
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+    next();
 });
 
+console.log('create server')
 const server = http.createServer(app);
 const io = socketio.listen(server, {log:false, origins:'*:*'});
 
 io.on('connection', (socket) => {
-  socket.on('message', (msg) => {
-    socket.broadcast.emit('message', msg);
-  });
+socket.on('message', (msg) => {
+socket.broadcast.emit('message', msg);
+});
 });
 
 server.listen(5000);
+
+console.log('server start on port')
+
+}catch(err) {console.log('error')}
